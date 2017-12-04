3c0b770eb1deb080d6385d2fde25edf02678b074
Ernesto Barros <e.leon.barros@gmail.com>
2017-12-04 08:16:00 +0100

    100% completion DAY03

--- src/day03.app/Program.fs
+++ src/day03.app/Program.fs
@@ -4,11 +4,25 @@
     | L // Left
     | D // Down
 
+
+let sumAdjacentSquares (x, y) spiral =
+    let adjacentSquares =
+        [
+            for x' in -1 .. 1 do
+            for y' in -1 .. 1 do
+            yield (x + x', y + y')
+        ] |> List.filter(fun (x', y') -> (x', y') <> (x, y))
+
+    spiral
+    |> List.filter(fun (_, _, (x', y'), _, _) -> adjacentSquares |> List.contains(x', y'))
+    |> List.sumBy(fun (_, v, _, _, _) -> v)
+
+
 let growSpiral spiral =
     match spiral with
     | [] ->  []
-    | (v, (x, y), d, ws) :: _' ->
-        let v' = v + 1
+    | (i, v, (x, y), d, ws) :: _' ->
+        let i' = i + 1
         // x': new x position
         let x' =
             // d: previous direction
@@ -26,7 +40,7 @@ let growSpiral spiral =
 
         let d' =
             // Turn point reached end of the road
-            if v' = List.sum(ws) + 1 then
+            if i' = List.sum(ws) + 1 then
                 match d with
                     | R -> U
                     | U -> L
@@ -35,49 +49,44 @@ let growSpiral spiral =
             else d
         let ws' =
             // Change of direction, Turn point reached end of the road
-            if v' = List.sum(ws) + 1 then
+            if i' = List.sum(ws) + 1 then
                 match d' with
                     | L
                     | R -> (ws.Head + 1) :: [List.sum(ws)]
                     | _ -> ws.Head :: [List.sum(ws)]
             else ws
+        let v' = sumAdjacentSquares (x', y') spiral
         // Return the new spiral
-        (v', (x', y'), d', ws') :: spiral
+        (i', v', (x', y'), d', ws') :: spiral
 
 
-let mkSpiral size =
-    let rec loop n spiral =
-        if n = size then List.rev spiral
-        else loop (n + 1) (growSpiral spiral)
-    loop 1 [1, (0, 0), R, [1]]
+let headValueSpiral spiral =
+    match spiral with
+    | [] ->  0
+    | (_, v, (_, _), _, _) :: _' -> v
 
-let manhattenDistance square =
-    let size = square
-    mkSpiral size
-    |> List.find(fun (x, _, _, _) -> x = square)
-    |> fun (_, (x, y), _, _) -> abs x + abs y
-    |> (printfn "Distance between Square %i and Square 1 = %i" square)
+
+let mkSpiral thresholdValue =
+    let rec loop spiral =
+        if (headValueSpiral spiral) > thresholdValue then List.rev spiral
+        else loop (growSpiral spiral)
+    loop [1, 1, (0, 0), R, [1]]
+
+
+let printSpiral thresholdValue =
+    mkSpiral thresholdValue
+    |> List.last |> (printfn "%A")
+
 
 [<EntryPoint>]
 let main _ =
-    manhattenDistance 1
-    manhattenDistance 12
-    manhattenDistance 23
-    manhattenDistance 1024
-    manhattenDistance 265149
+    printSpiral (265149)
     0 // return an integer exit code
 
 (*
-17  16  15  14  13
-18   5   4   3  12
-19   6   1   2  11
-20   7   8   9  10
-21  22  23---> ...
-
-For example:
-Data from square 1 is carried 0 steps, since it's at the access port.
-Data from square 12 is carried 3 steps, such as: down, left, left.
-Data from square 23 is carried only 2 steps: up twice.
-Data from square 1024 must be carried 31 steps.
-
+147  142  133  122   59
+304    5    4    2   57
+330   10    1    1   54
+351   11   23   25   26
+362  747  806--->   ...
 *)
